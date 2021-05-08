80f4aca101a2f4e500b0c48d6a7423b9c2948b90
mayurwabale <83824245+mayurwabale@users.noreply.github.com>
2021-05-08 16:25:06 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class addition
+{
+public static void main(String args[]){
+System.out.println("Enter No 1");
+Scanner sc=new Scanner(System.in);
+int a = sc.nextInt();
+System.out.println("Enter No 2");
+int b = sc.nextInt();
+int c = a+b;
+System.out.println("Sum is");
+System.out.println(c);
+}
+}
